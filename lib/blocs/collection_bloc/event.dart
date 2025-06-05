417a4187785e23aeb1a93304695804eaede009e5
abdulrehman04 <abdulrehman71254@gmail.com>
2025-06-05 21:42:56 +0200

    feat: sertup collection events

---
+++ lib/blocs/collection_bloc/event.dart
@@ -0,0 +1,10 @@
+import 'package:equatable/equatable.dart';
+
+class CollectionEvents extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class FetchCollectionEvent extends CollectionEvents {
+  FetchCollectionEvent();
+}
