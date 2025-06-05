4354c7908d1263465775ed99c537785a4650eb55
abdulrehman04 <abdulrehman71254@gmail.com>
2025-06-06 00:21:05 +0200

    chore: created collection name

---
+++ lib/screens/details/widgets/_collection_name.dart
@@ -0,0 +1,37 @@
+part of '../details.dart';
+
+class CollectionName extends StatelessWidget {
+  const CollectionName({super.key, required this.collectionName});
+
+  final String collectionName;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 24.h, horizontal: 16.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            color: AppTheme.kDarkbgColor,
+            padding: EdgeInsets.symmetric(vertical: 4.h, horizontal: 8.w),
+            child: Text(
+              collectionName,
+              style: Theme.of(
+                context,
+              ).textTheme.bodySmall?.copyWith(color: AppTheme.kGrey1Color),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(13.2),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: AppTheme.kDarkbgColor,
+            ),
+            child: Icon(Icons.close, color: AppTheme.kGrey1Color, size: 24),
+          ),
+        ],
+      ),
+    );
+  }
+}
